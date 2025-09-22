872856951928a0298e0bcb8e0e8fc97c3a256c4a
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-22 17:49:49 +0530

    Pattern 04

---
+++ src/patternProgramming/Pattern_04.java
@@ -0,0 +1,38 @@
+package patternProgramming;
+
+import java.util.Scanner;
+
+// Fourth Pattern Problem.
+public class Pattern_04 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		if (n <= 0) {
+			System.out.println("Please enter a positive number!");
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (j % 2 == 0) {
+					System.out.print("0 ");
+				} else {
+					System.out.print("1 ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
+// Here is the Pattern : 
+
+//1 0 1 0 1 
+//1 0 1 0 1 
+//1 0 1 0 1 
+//1 0 1 0 1 
+//1 0 1 0 1 
