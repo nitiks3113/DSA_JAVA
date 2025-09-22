c4009166fcc8063cbd061ef6ba4a9bfad6bad53b
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-22 18:44:48 +0530

    Pattern 10 Flag

---
+++ src/patternProgramming/Pattern_10.java
@@ -0,0 +1,53 @@
+package patternProgramming;
+
+import java.util.Scanner;
+
+public class Pattern_10 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		int mid = n / 2;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i == mid + 1 || j == mid + 1 || i == j || j == (n - i + 1) || i == 1 || i == n || j == 1
+						|| j == n) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		for (int i = 1; i <= n; i++) {
+			System.out.println("**");
+		}
+	}
+
+}
+// Here is the Pattern : 
+//* * * * * * * * * * * 
+//* *       *       * * 
+//*   *     *     *   * 
+//*     *   *   *     * 
+//*       * * *       * 
+//* * * * * * * * * * * 
+//*       * * *       * 
+//*     *   *   *     * 
+//*   *     *     *   * 
+//* *       *       * * 
+//* * * * * * * * * * * 
+//**
+//**
+//**
+//**
+//**
+//**
+//**
+//**
+//**
+//**
+**
