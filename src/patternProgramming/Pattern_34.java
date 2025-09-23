7d380f7a41062262ad875598b1dfbf3c9930a421
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-23 23:20:26 +0530

    Pattern 34

---
+++ src/patternProgramming/Pattern_34.java
@@ -0,0 +1,39 @@
+package patternProgramming;
+
+// Thirty-fourth Pattern Problem
+import java.util.Scanner;
+public class Pattern_34 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the no. of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("  ");
+			}
+
+			// Print digits
+			for (int k = 1; k <= i; k++) {
+				if (i % 2 == 0) {
+					System.out.print("0 ");
+				} else {
+					System.out.print("1 ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
+// Here is the Pattern : 
+//            1 
+//          0 0 
+//        1 1 1 
+//      0 0 0 0 
+//    1 1 1 1 1 
+//  0 0 0 0 0 0 
+//1 1 1 1 1 1 1 
