64929a034a5b175bb599387abb862369ab2edc3a
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-23 19:19:32 +0530

    Pattern 19

---
+++ src/patternProgramming/Pattern_19.java
@@ -0,0 +1,32 @@
+package patternProgramming;
+
+import java.util.Scanner;
+
+// Nineteenth Pattern Problem 
+
+public class Pattern_19 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the no. of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+// Here is the Pattern :
+// Enter the no. of rows : 7
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+// 1 2 3 4 5 6 7
