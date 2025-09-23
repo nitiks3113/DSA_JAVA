af9531404fe57d2bb40d9f0844429acd9144a73d
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-23 19:12:55 +0530

    Pattern 17

---
+++ src/patternProgramming/Pattern_17.java
@@ -0,0 +1,30 @@
+package patternProgramming;
+
+// Seventeenth Pattern Problem
+import java.util.Scanner;
+public class Pattern_17 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the no. of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+// Here is the Pattern :
+// Enter the no. of rows : 7
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+// 6 6 6 6 6 6
+// 7 7 7 7 7 7 7
