7ae8647f0f00a06abd3c3fffa35525db71ec20a0
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-23 18:56:56 +0530

    Pattern 14

---
+++ src/patternProgramming/Pattern_14.java
@@ -0,0 +1,30 @@
+package patternProgramming;
+
+// Fourteenth Pattern Problem 
+import java.util.Scanner;
+public class Pattern_14 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the no. of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+// Here is the Pattern :
+// Enter the no. of rows : 7
+// * * * * * * *
+// * * * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
