564b24b08fce1e5f9400751327f870bb2a78487d
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-24 21:11:02 +0530

    Pattern 55 Binary Combination Pyramid

---
+++ src/patternProgramming/Pattern_55.java
@@ -0,0 +1,37 @@
+package patternProgramming;
+
+// Fifty-fifth Pattern Problem 
+import java.util.Scanner;
+public class Pattern_55 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			// Print Binary Combination Pyramid
+			for (int k = 1; k <= i; k++) {
+				if (k % 2 == 0) {
+					System.out.print("0 ");
+				} else {
+					System.out.print("1 ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
+// Here is the Pattern : 
+//    1 
+//   1 0 
+//  1 0 1 
+// 1 0 1 0 
+//1 0 1 0 1 
