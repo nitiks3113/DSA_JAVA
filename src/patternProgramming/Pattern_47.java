212ad7e3766f0f58b32c2b5dbcfbae92e59702d1
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-24 18:11:45 +0530

    Pattern 47

---
+++ src/patternProgramming/Pattern_47.java
@@ -0,0 +1,35 @@
+package patternProgramming;
+
+// Forty-seventh Pattern Problem
+import java.util.Scanner;
+public class Pattern_47 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		char ch = (char) (64 + n);
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("   ");
+			}
+
+			// Print Characters
+			for (int k = 1; k <= i; k++) {
+				System.out.printf("%3c", ch);
+			}
+			ch--;
+			System.out.println();
+		}
+	}
+
+}
+// Here is the Pattern : 
+//            E
+//         D  D
+//      C  C  C
+//   B  B  B  B
+//A  A  A  A  A
