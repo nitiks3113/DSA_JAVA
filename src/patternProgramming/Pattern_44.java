236bb0908ffce02c5ecab2e5462211af8cf917ca
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-24 17:51:28 +0530

    Pattern 44

---
+++ src/patternProgramming/Pattern_44.java
@@ -0,0 +1,34 @@
+package patternProgramming;
+
+// Forty-fourth Pattern Problem
+import java.util.Scanner;
+public class Pattern_44 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the no. of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		for (int i = 1; i <= n; i++) {
+			char ch = 'A';
+			// Print Spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("   ");
+			}
+
+			// Print Characters
+			for (int k = 1; k <= i; k++) {
+				System.out.printf("%3c", ch++);
+			}
+			System.out.println();
+		}
+	}
+
+}
+// Here is the Pattern : 
+//            A
+//         A  B
+//      A  B  C
+//   A  B  C  D
+//A  B  C  D  E
