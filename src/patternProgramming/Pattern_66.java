802115fd7b33a99a0cebc24a0cc2e98eb3b76f5d
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-25 14:48:20 +0530

    Pattern 66 Characters Pyramid

---
+++ src/patternProgramming/Pattern_66.java
@@ -0,0 +1,39 @@
+package patternProgramming;
+
+// Sixty-sixth Pattern Problem 
+import java.util.Scanner;
+public class Pattern_66 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		char start = (char) (64 + (n * (n + 1) / 2));
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Characters
+			for (int k = 1; k <= i; k++) {
+				System.out.printf("%-4c", start--);
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+// Here is the Pattern : 
+//        O   
+//      N   M   
+//    L   K   J   
+//  I   H   G   F   
+//E   D   C   B   A 
