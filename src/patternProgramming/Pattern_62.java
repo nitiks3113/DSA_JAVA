c19482c63dede974941ddb39b0a569f7edcbe296
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-25 14:20:09 +0530

    Pattern 62 Characters Pyramid

---
+++ src/patternProgramming/Pattern_62.java
@@ -0,0 +1,39 @@
+package patternProgramming;
+
+// Sixty-second Pattern Problem 
+import java.util.Scanner;
+public class Pattern_62 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		for (int i = 1; i <= n; i++) {
+			char ch = 'A';
+			// Print Spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Characters
+			for (int k = 1; k <= i; k++) {
+				System.out.printf("%-4c", ch++);
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+// Here is the Pattern : 
+//        A   
+//      A   B   
+//    A   B   C   
+//  A   B   C   D   
+//A   B   C   D   E 
