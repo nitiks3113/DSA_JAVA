643c2f177cc92773cc13213a5ce790c03df7cf41
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-25 14:31:26 +0530

    Pattern 65 Characters Pyramid

---
+++ src/patternProgramming/Pattern_65.java
@@ -0,0 +1,39 @@
+package patternProgramming;
+
+// Sixty-fifth Pattern Problem 
+import java.util.Scanner;
+public class Pattern_65 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		char ch = 'A';
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Characters
+			for (int k = 1; k <= i; k++) {
+				System.out.printf("%-4c", ch++);
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+// Here is the Pattern : 
+//        A   
+//      B   C   
+//    D   E   F   
+//  G   H   I   J   
+//K   L   M   N   O 
