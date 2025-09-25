d482e36d65fe1e54be643f9df228c3c93dc58d0f
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-25 18:11:37 +0530

    Pattern 79 Number Pyramid

---
+++ src/patternProgramming/Pattern_79.java
@@ -0,0 +1,45 @@
+package patternProgramming;
+
+// Seventy-nineth Pattern Problem 
+import java.util.Scanner;
+public class Pattern_79 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = 1;
+		for (int i = 1; i <= n; i++) {
+			int num = start;
+			// Print Spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Numbers
+			for (int k = 1; k <= 2 * i - 1; k++) {
+				int mid = (2 * i - 1) / 2 + 1;
+				if (k < mid) {
+					System.out.print(num-- + " ");
+				} else {
+					System.out.print(num++ + " ");
+				}
+			}
+			start++;
+			System.out.println();
+		}
+	}
+}
+
+// Here is the Pattern : 
+//        1 
+//      2 1 2 
+//    3 2 1 2 3 
+//  4 3 2 1 2 3 4 
+//5 4 3 2 1 2 3 4 5 
