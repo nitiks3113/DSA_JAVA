fcef06b2f232db9ad12e113edf5a6d803549d562
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-26 21:39:13 +0530

    Pattern 100 Inverted Pyramid

---
+++ src/patternProgramming/Pattern_100.java
@@ -0,0 +1,38 @@
+package patternProgramming;
+
+// Hundredth Pattern Problem 
+import java.util.Scanner;
+public class Pattern_100 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = n * (n + 1) / 2;
+		for (int i = 1; i <= n; i++) {
+			// Print Pattern
+			for (int j = 1; j < i; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Number
+			for (int k = 1; k <= n - i + 1; k++) {
+				System.out.printf("%-4d", start--);
+			}
+			System.out.println();
+		}
+	}
+}
+
+// Here is the Pattern : 
+//15  14  13  12  11  
+//  10  9   8   7   
+//    6   5   4   
+//      3   2   
+//        1  
