2eef71080704ab2b8920b843b9c6d08596f432e2
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-26 21:09:12 +0530

    Pattern 97 Inverted Pyramid

---
+++ src/patternProgramming/Pattern_97.java
@@ -0,0 +1,38 @@
+package patternProgramming;
+
+// Ninety-seventh Pattern Problem 
+import java.util.Scanner;
+public class Pattern_97 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		for (int i = 1; i <= n; i++) {
+			int start = 1;
+			// Print Spaces
+			for (int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+
+			// Print Numbers
+			for (int k = 1; k <= n - i + 1; k++) {
+				System.out.print(start++ + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+// Here is the Pattern : 
+//1 2 3 4 5 
+// 1 2 3 4 
+//  1 2 3 
+//   1 2 
+//    1 
