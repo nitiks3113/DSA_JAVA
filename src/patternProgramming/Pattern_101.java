c77c9a61e0d9c72c838ca06249aab551c120f3e2
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-26 21:44:15 +0530

    Pattern 101 Inverted Characters Pyramid

---
+++ src/patternProgramming/Pattern_101.java
@@ -0,0 +1,39 @@
+package patternProgramming;
+
+// Hundred One Pattern Problem 
+import java.util.Scanner;
+public class Pattern_101 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		char ch = 'A';
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j < i; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Characters
+			for (int k = 1; k <= n - i + 1; k++) {
+				System.out.printf("%-4c", ch);
+			}
+			ch++;
+			System.out.println();
+		}
+	}
+}
+
+// Here is the Pattern : 
+//A   A   A   A   A   
+//  B   B   B   B   
+//    C   C   C   
+//      D   D   
+//        E   
