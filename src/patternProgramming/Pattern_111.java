717176e5bb6e08be342db9676b4d1f1978efc46c
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-26 23:12:46 +0530

    Pattern 111 Inverted Binary Pyramid

---
+++ src/patternProgramming/Pattern_111.java
@@ -0,0 +1,39 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_111 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j < i; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Binary
+			for (int k = 1; k <= (2 * (n - i + 1) - 1); k++) {
+				if (k % 2 == 0)
+					System.out.printf("%-2d", 0);
+				else
+					System.out.printf("%-2d", 1);
+			}
+			System.out.println();
+		}
+	}
+}
+
+// Here is the Pattern : 
+//1 0 1 0 1 0 1 0 1 
+//  1 0 1 0 1 0 1 
+//    1 0 1 0 1 
+//      1 0 1 
+//        1 
