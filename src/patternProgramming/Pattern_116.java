b26533a0953e3643d21a3d89a3d63406608c0a05
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-27 07:57:49 +0530

    Pattern 116 Inverted Number Pyramid

---
+++ src/patternProgramming/Pattern_116.java
@@ -0,0 +1,41 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_116 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		for (int i = 1; i <= n; i++) {
+			int start = 1;
+			// Print Spaces
+			for (int j = 1; j < i; j++) {
+				System.out.print("    ");
+			}
+
+			// Print Numbers
+			for (int k = 1; k <= (2 * (n - i + 1) - 1); k++) {
+				int mid = (2 * (n - i + 1) - 1) / 2 + 1;
+				if (k < mid)
+					System.out.printf("%-4d", start++);
+				else
+					System.out.printf("%-4d", start--);
+			}
+			System.out.println();
+		}
+	}
+}
+
+// Here is the Pattern : 
+//1   2   3   4   5   4   3   2   1   
+//	1   2   3   4   3   2   1   
+//    	1   2   3   2   1   
+//        	1   2   1   
+//            	1   
