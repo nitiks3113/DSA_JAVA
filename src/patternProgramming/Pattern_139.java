31a491003ba1169b62c733cab436c11bfeb01ca7
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-28 16:36:16 +0530

    Pattern 139

---
+++ src/patternProgramming/Pattern_139.java
@@ -0,0 +1,53 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_139 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int mid = n / 2 + 1;
+		int space = n / 2;
+		int start = mid - 1;
+		int num = 1;
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= space; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Numbers
+			for (int j = 1; j <= num; j++) {
+				System.out.printf("%-4d", start);
+			}
+			System.out.println();
+			if (i < mid) {
+				start--;
+				space--;
+				num++;
+			} else {
+				start++;
+				space++;
+				num--;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+/*
+	  3   
+	2   2   
+  1   1   1   
+0   0   0   0   
+  1   1   1   
+	2   2   
+	  3 
+*/
