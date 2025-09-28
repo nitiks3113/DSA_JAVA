5cfd5a8460fae5fb23470fa3134de3b4e5be897c
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-28 08:20:14 +0530

    Pattern 131 Number Diamond

---
+++ src/patternProgramming/Pattern_131.java
@@ -0,0 +1,54 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_131 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = 1;
+		int space = n / 2;
+		int mid = n / 2 + 1;
+		int star = start;
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= space; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Numbers
+			for (int j = 1; j <= star; j++) {
+				System.out.print(start + "   ");
+			}
+
+			System.out.println();
+
+			if (i < mid) {
+				star++;
+				start++;
+				space--;
+			} else {
+				star--;
+				start++;
+				space++;
+			}
+		}
+	}
+}
+
+
+// Here is the Pattern : 
+//	    1   
+//	  2   2   
+//  3   3   3   
+//4   4   4   4   
+//  5   5   5   
+//	  6   6   
+//	    7 
