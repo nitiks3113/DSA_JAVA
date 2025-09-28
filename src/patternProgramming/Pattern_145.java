7ec0999d50e107edbb35315942582a378da6396b
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-28 17:07:38 +0530

    Pattern 145

---
+++ src/patternProgramming/Pattern_145.java
@@ -0,0 +1,52 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_145 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int mid = n / 2 + 1;
+		int start = 1;
+		int space = n / 2;
+		char ch = 'A';
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= space; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Characters
+			for (int j = 1; j <= start; j++) {
+				System.out.printf("%-4c", ch++);
+			}
+
+			System.out.println();
+			if (i < mid) {
+				start++;
+				space--;
+			} else {
+				start--;
+				space++;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+/*
+	  A   
+	B   C   
+  D   E   F   
+G   H   I   J   
+  K   L   M   
+	N   O   
+	  P  
+*/
