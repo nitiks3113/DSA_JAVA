ad125908e25ab677b3258fc29f531d4d2e4b0aa0
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-28 16:30:37 +0530

    Pattern 138

---
+++ src/patternProgramming/Pattern_138.java
@@ -0,0 +1,52 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_138 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int mid = n / 2 + 1;
+		int space = n / 2;
+		int start = n;
+		int num = 1;
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= space; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Numbers
+			for (int j = 1; j <= num; j++) {
+				System.out.printf("%-4d", start);
+			}
+			System.out.println();
+			start--;
+			if (i < mid) {
+				num++;
+				space--;
+			} else {
+				num--;
+				space++;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+/* 
+	  7   
+	6   6   
+  5   5   5   
+4   4   4   4   
+  3   3   3   
+	2   2   
+	  1 
+*/
