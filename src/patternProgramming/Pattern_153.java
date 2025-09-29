34d6c874a586b2bea3fdb45d315a66c8c68489cc
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-29 22:13:18 +0530

    Pattern 153

---
+++ src/patternProgramming/Pattern_153.java
@@ -0,0 +1,51 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_153 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = 1;
+		int mid = n / 2 + 1;
+		int space = n / 2;
+		int num = ((n + 1) / 2) * ((n + 1) / 2) + (n / 2) * (n / 2);
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= space; j++) {
+				System.out.print("    ");
+			}
+
+			// Print Pattern
+			for (int j = 1; j <= start; j++) {
+				System.out.printf("%-4d", num--);
+			}
+			System.out.println();
+			if (i < mid) {
+				start += 2;
+				space--;
+			} else {
+				start -= 2;
+				space++;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+/*
+			25  
+		24  23  22  
+	21  20  19  18  17  
+16  15  14  13  12  11  10  
+	9   8   7   6   5   
+		4   3   2   
+  			1 
+*/
