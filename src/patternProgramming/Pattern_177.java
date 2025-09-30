b7b13ef0fa971043d44af4f82150c2d4571db4ea
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-30 22:05:46 +0530

    Pattern 177

---
+++ src/patternProgramming/Pattern_177.java
@@ -0,0 +1,53 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_177 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = 1;
+		int space = n / 2;
+		int mid = n / 2 + 1;
+		char ch = 'A';
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= space; j++) {
+				System.out.print("    ");
+			}
+
+			// Print Pattern
+			for (int j = 1; j <= start; j++) {
+				System.out.printf("%-4c", ch);
+			}
+			System.out.println();
+			if (i < mid) {
+				ch++;
+				start += 2;
+				space--;
+			} else {
+				ch--;
+				start -= 2;
+				space++;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+/*
+            A   
+        B   B   B   
+    C   C   C   C   C   
+D   D   D   D   D   D   D   
+    C   C   C   C   C   
+        B   B   B   
+            A 
+*/
