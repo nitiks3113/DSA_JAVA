ef400d6856d46eeffcb59e4e50a6c0ef3528e82a
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-09-30 21:49:16 +0530

    Pattern 172

---
+++ src/patternProgramming/Pattern_172.java
@@ -0,0 +1,55 @@
+package patternProgramming;
+
+import java.util.Scanner;
+
+public class Pattern_172 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = 1;
+		int mid = n / 2 + 1;
+		int space = n / 2;
+		int num = 1;
+		for (int i = 1; i <= n; i++) {
+			// Print Pattern
+			for (int j = 1; j <= space; j++) {
+				System.out.print("    ");
+			}
+
+			// Print Pattern
+			for (int j = 1; j <= start; j++) {
+				if (i == mid || j == (start / 2) + 1 && i < n)
+					System.out.printf("%-4c", '@');
+				else if (i < n)
+					System.out.printf("%-4d", num++);
+			}
+
+			System.out.println();
+			if (i < mid) {
+				start += 2;
+				space--;
+			} else {
+				start -= 2;
+				space++;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+/*
+            @   
+        1   @   2   
+    3   4   @   5   6   
+@   @   @   @   @   @   @   
+    7   8   @   9   10  
+        11  @   12 
+*/
