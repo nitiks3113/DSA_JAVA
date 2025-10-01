543ed741daea675bbba9364e58a92c76b620bc6e
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-01 18:28:13 +0530

    Pattern 184

---
+++ src/patternProgramming/Pattern_184.java
@@ -0,0 +1,52 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_184 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = n;
+		int space = 0;
+		int mid = n / 2 + 1;
+		for (int i = 1; i <= n; i++) {
+			int num = 1;
+			// Print Spaces
+			for (int j = 1; j <= space; j++) {
+				System.out.print("  ");
+			}
+
+			// Print Pattern
+			for (int j = 1; j <= start; j++) {
+				System.out.print(num++ + " ");
+			}
+			System.out.println();
+			if (i < mid) {
+				start -= 2;
+				space++;
+			} else {
+				start += 2;
+				space--;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+
+/*
+1 2 3 4 5 6 7 
+  1 2 3 4 5 
+    1 2 3 
+      1 
+    1 2 3 
+  1 2 3 4 5 
+1 2 3 4 5 6 7
+*/
