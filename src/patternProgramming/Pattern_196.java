b10b06326a4dfa819038b34c9c2611654519c2b8
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-01 19:09:19 +0530

    Pattern 196

---
+++ src/patternProgramming/Pattern_196.java
@@ -0,0 +1,53 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_196 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = n;
+		int space = 0;
+		int mid = n / 2 + 1;
+		char ch = 'A';
+		for (int i = 1; i <= n; i++) {
+			// Print Spaces
+			for (int j = 1; j <= space; j++) {
+				System.out.print("   ");
+			}
+
+			// Print Pattern
+			for (int j = 1; j <= start; j++) {
+				System.out.printf("%-3c", ch);
+			}
+			ch++;
+			System.out.println();
+			if (i < mid) {
+				start -= 2;
+				space++;
+			} else {
+				start += 2;
+				space--;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+
+/*
+A  A  A  A  A  A  A  
+   B  B  B  B  B  
+      C  C  C  
+         D  
+      E  E  E  
+   F  F  F  F  F  
+G  G  G  G  G  G  G 
+*/
