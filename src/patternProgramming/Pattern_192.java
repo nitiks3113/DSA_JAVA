d6f5abefda56b9f03ddab038d45837a765a42f91
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-01 19:02:36 +0530

    Pattern 192

--- src/patternProgramming/Pattern_192.java
+++ src/patternProgramming/Pattern_192.java
@@ -18,7 +18,6 @@ public class Pattern_192 {
 		int mid = n / 2 + 1;
 		int num = mid - 1;
 		for (int i = 1; i <= n; i++) {
-			int nums = num;
 			// Print Spaces
 			for (int j = 1; j <= space; j++) {
 				System.out.print("   ");
@@ -42,14 +41,8 @@ public class Pattern_192 {
 	}
 }
 
-// Here is the Pattern : 
+// Here is the Pattern :
 
 /*
-3  3  3  3  3  3  3  
-   2  2  2  2  2  
-      1  1  1  
-         0  
-      1  1  1  
-   2  2  2  2  2  
-3  3  3  3  3  3  3  
-*/
+ * 3 3 3 3 3 3 3 2 2 2 2 2 1 1 1 0 1 1 1 2 2 2 2 2 3 3 3 3 3 3 3
+ */
