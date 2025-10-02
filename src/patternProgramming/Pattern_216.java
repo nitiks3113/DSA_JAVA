35d4cbd7eaade97d3495388494ad3001b3f25969
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-02 16:27:56 +0530

    Pattern 216

---
+++ src/patternProgramming/Pattern_216.java
@@ -0,0 +1,38 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_216 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = 0;
+		int gap = 2;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				int nextNum = start + gap;
+				System.out.printf("%-4d", nextNum);
+				start = nextNum;
+				gap += 2;
+			}
+			System.out.println();
+		}
+	}
+}
+
+// Here is the Pattern : 
+
+/*
+2   
+6   12  
+20  30  42  
+56  72  90  110 
+132 156 182 210 240 
+*/
