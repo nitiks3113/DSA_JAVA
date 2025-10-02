16476bc38a3edd7fd0bceb74fe4dab73f1c77320
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-02 15:31:36 +0530

    Pattern 209

---
+++ src/patternProgramming/Pattern_209.java
@@ -0,0 +1,51 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_209 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int start = 1;
+		int end = n;
+		int mid = n / 2 + 1;
+		char ch = 'A';
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (j <= start || j >= end) {
+					System.out.printf("%-3c", ch);
+				} else {
+					System.out.print("   ");
+				}
+			}
+			ch++;
+			System.out.println();
+			if (i < mid) {
+				start++;
+				end--;
+			} else {
+				start--;
+				end++;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+
+/*
+A                 A  
+B  B           B  B  
+C  C  C     C  C  C  
+D  D  D  D  D  D  D  
+E  E  E     E  E  E  
+F  F           F  F  
+G                 G  
+*/
