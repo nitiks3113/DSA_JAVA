bd3879bff9750055ff1ce4019405500388379a47
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-03 20:16:46 +0530

    Pattern 235

---
+++ src/patternProgramming/Pattern_235.java
@@ -0,0 +1,50 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_235 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int sStar = n / 2 + 1;
+		int eStar = n / 2 + 1;
+		int mid = n / 2 + 1;
+		int num = 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= eStar; j++) {
+				if (j == sStar || j == eStar)
+					System.out.print(num + " ");
+				else
+					System.out.print("  ");
+			}
+			num++;
+			System.out.println();
+			if (i < mid) {
+				sStar--;
+				eStar++;
+			} else {
+				sStar++;
+				eStar--;
+			}
+		}
+	}
+}
+
+// Here is the Pattern : 
+
+/*
+      1 
+    2   2 
+  3       3 
+4           4 
+  5       5 
+    6   6 
+      7 
+*/
