44fd40a7e39608ca7acb0af7146f419dff91c0df
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-03 19:49:21 +0530

    Pattern 230

---
+++ src/patternProgramming/Pattern_230.java
@@ -0,0 +1,43 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_230 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int sStar = n;
+		int eStar = n;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= eStar; j++) {
+				if (j == sStar || j == eStar || i == n
+						|| j == (sStar + eStar) / 2)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+			eStar++;
+			sStar--;
+		}
+	}
+}
+
+// Here is the Pattern : 
+
+/*
+            * 
+          * * * 
+        *   *   * 
+      *     *     * 
+    *       *       * 
+  *         *         * 
+* * * * * * * * * * * * * 
+*/
