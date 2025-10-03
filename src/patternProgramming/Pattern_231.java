1dc54221f3c72ecc85179fac610198e67321f828
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-03 19:57:09 +0530

    Pattern 231

---
+++ src/patternProgramming/Pattern_231.java
@@ -0,0 +1,42 @@
+package patternProgramming;
+
+import java.util.Scanner;
+public class Pattern_231 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows : ");
+		int n = sc.nextInt();
+		sc.close();
+		printPattern(n);
+	}
+
+	public static void printPattern(int n) {
+		int sStar = 1;
+		int eStar = 2 * n - 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= eStar; j++) {
+				if (j == sStar || j == eStar)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+			eStar--;
+			sStar++;
+		}
+	}
+}
+
+// Here is the Pattern : 
+
+/*
+*                       * 
+  *                   * 
+    *               * 
+      *           * 
+        *       * 
+          *   * 
+            * 
+*/
