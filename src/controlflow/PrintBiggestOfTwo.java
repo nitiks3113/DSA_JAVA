f1c86868372ec3c4e641ed7ae3d2eac0ffb89fd7
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-10 21:03:13 +0530

    Print Biggest of Two

---
+++ src/controlflow/PrintBiggestOfTwo.java
@@ -0,0 +1,25 @@
+package controlflow;
+
+// WAJP to print big of two numbers using if else statement.
+import java.util.Scanner;
+
+public class PrintBiggestOfTwo {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first number : ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter the second number : ");
+		int num2 = sc.nextInt();
+		if (num1 > num2) {
+			System.out.println(num1 + " is greater than " + num2);
+		} else if (num2 > num1) {
+			System.out.println(num2 + " is greater than " + num1);
+		} else {
+			System.out.println("They are equal.");
+		}
+		sc.close();
+	}
+
+}
