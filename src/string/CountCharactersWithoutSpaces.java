a5eb70573d6a11c0bef01ea067f4d91551f75c4a
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-10-14 20:19:52 +0530

    Count Characters Without Counting Spaces

---
+++ src/string/CountCharactersWithoutSpaces.java
@@ -0,0 +1,26 @@
+package string;
+
+// WAJP to take a String input and count all the characters without spaces in the String.
+import java.util.Scanner;
+public class CountCharactersWithoutSpaces {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the String : ");
+		String str = sc.nextLine();
+		sc.close();
+		int countCharacters = countCharacters(str);
+		System.out.println("Total number of characters without spaces is : "
+				+ countCharacters);
+	}
+
+	public static int countCharacters(String str) {
+		int count = 0;
+		for (char c : str.toCharArray()) {
+			if (c != ' ')
+				count++;
+		}
+		return count;
+	}
+}
