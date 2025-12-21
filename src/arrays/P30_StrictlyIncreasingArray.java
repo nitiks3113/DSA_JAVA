3ff0d1a880a19f097e19fc54020ec846c1fa75ff
Nitik Kumar Sahay <nitikkrsahay3113@gmail.com>
2025-12-21 19:55:38 +0530

    Strictly Increasing Array

---
+++ src/arrays/P30_StrictlyIncreasingArray.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.*;
+
+public class P30_StrictlyIncreasingArray {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size of the Array : ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter array elements : ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		System.out.println("Array is : " + Arrays.toString(arr));
+		checkStrictlyIncreasingArray(arr);
+	}
+
+	public static void checkStrictlyIncreasingArray(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			if (arr[i] >= arr[i + 1]) {
+				System.out.println("Array is not Strictly Increasing...");
+				return;
+			}
+		}
+
+		System.out.println("Array is Strictly Increasing...");
+	}
+}
